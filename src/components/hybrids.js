de20ba09f679f34b2e93f54f727bc6f07e2fbca2
Alex Waller <alex@artofalexwaller.com>
2021-03-15 17:41:11 -0400

    HTML JS Source

--- src/components/hybrids.js
+++ src/components/hybrids.js
@@ -3,5 +3,12 @@ class Hybrids {
         this.hybrids = []
         this.adapter = new HybridsAdapter()
         this.bindEventListeners()
+        this.fetchAndLoadHybrids()
+    }
+
+    fetchAndLoadHybrids(){
+        this.adapter.getHybrids().then(hybrids => {
+            console.log(hybrids)
+        })
     }
 }

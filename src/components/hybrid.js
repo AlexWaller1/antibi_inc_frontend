b8d11b2c080b73c34c7fcae1c918a8483dc4b485
Alex Waller <alex@artofalexwaller.com>
2021-03-15 19:49:50 -0400

    hybrid.js

---
+++ src/components/hybrid.js
@@ -0,0 +1,11 @@
+class Hybrid {
+    constructor(hybridJSON) {
+        this.id = hybridJSON.id
+        this.name = hybridJSON.name
+        this.species = hybridJSON.species
+        this.personality = hybridJSON.personality
+        this.biography = hybridJSON.biography
+        this.image = hybridJSON.image
+        
+    }
+}

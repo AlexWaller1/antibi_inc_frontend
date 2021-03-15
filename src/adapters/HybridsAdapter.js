ce80529fd7f0be80b30bb0a10825f5f58dd8af7d
Alex Waller <alex@artofalexwaller.com>
2021-03-15 17:04:31 -0400

    first fetch request

--- src/adapters/HybridsAdapter.js
+++ src/adapters/HybridsAdapter.js
@@ -1,4 +1,11 @@
 // will talk to backend Api
 class HybridsAdapter {
-    constructor()
+    constructor() {
+        this.baseUrl = 'http://localhost:3000/api/v1/hybrids'
+    }
+
+    getHybrids() {
+        return fetch(this.baseUrl).then(res => res.json()
+        )
+    }
 }
